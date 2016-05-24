dd4cc554f3217b27aff7dfb7b3bad4cdc9769dea
Vardan <vard.1995@gmail.com>
2016-05-24 17:23:45 +0400

    pushov cakelem

--- js/open_house.js
+++ js/open_house.js
@@ -1,27 +1,74 @@
 /**
  * Created by Vardan on 23.05.2016.
  */
-$(".second_image, .third_image").addClass("hidden");
-$(".first_image").show();
-$(".slide_sticks .stick").removeClass("active_stick");
-$(".first_stick").addClass("active_stick");
 
-$(".first_stick").click(function () {
-    $(".slide_sticks .stick").removeClass("active_stick");
+
+$(" .slide__stick").click(function () {
+    $(".active_stick").removeClass("active_stick");
     $(this).addClass("active_stick");
+});
+
+
+$(".slide__first_stick").click(function () {
     $(".second_image, .third_image").hide();
     $(".first_image").show();
 });
-$(".second_stick").click(function () {
-    $(".slide_sticks .stick").removeClass("active_stick");
-    $(this).addClass("active_stick");
+
+$(".slide__second_stick").click(function () {
     $(".first_image, .third_image").hide();
     $(".second_image").show();
 });
-$(".third_stick").click(function () {
-    $(".slide_sticks .stick").removeClass("active_stick");
-    $(this).addClass("active_stick");
+
+
+$(".slide__third_stick").click(function () {
     $(".first_image, .second_image").hide();
     $(".third_image").show();
 });
-console.log("asdf");
+
+
+$(" .availability_menu_links").click(function () {
+    $(".availability__menu__active").removeClass("availability__menu__active");
+    $(this).addClass("availability__menu__active");
+});
+
+$(".all_links").click(function () {
+    $(".availability__section").show();
+    $(".image__slider, .floorplan").addClass("hide");
+});
+
+$(".studio_links").click(function () {
+    $(".one_bed__section, .two_bed__section, .photo_gallery, .floorplan").hide();
+    $(".studio__section").show();
+});
+
+$(".one_bed_links").click(function () {
+    $(".studio__section, .two_bed__section, .photo_gallery, .floorplan").hide();
+    $(".one_bed__section").show();
+});
+
+$(".two_bed_links").click(function () {
+    $(".studio__section, .one_bed__section,.photo_gallery, .floorplan").hide();
+    $(".two_bed__section").show();
+});
+
+
+$(".photo_gallery").addClass("hidden");
+
+$(".floorplan").addClass("hidden");
+
+$(".availability__section__fourth_item").click(function () {
+    $('.availability__section').not($(this).closest('.availability__section')).hide();
+    $(".floorplan").hide();
+    $(".photo_gallery").show();
+
+});
+
+
+$(".availability__section__fifth_item").click(function () {
+    $('.availability__section').not($(this).closest('.availability__section')).hide();
+    $(".photo_gallery").hide();
+    $(".floorplan").show();
+
+});
+
+
